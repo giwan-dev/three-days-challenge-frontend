ff2110bb69a0459e0fe6a623723783a81725ad7c
giwan <giwan.dev@gmail.com>
2022-12-06 07:34:43 +0900

    ✨ 완주 페이지 구현

---
+++ pages/complete.tsx
@@ -0,0 +1,83 @@
+import { useState } from "react";
+
+enum AccountInfoStatus {
+  Empty,
+  Filled,
+}
+
+export default function Complete() {
+  const [accuntInfoStatus, setAccountInfoStatus] = useState(
+    AccountInfoStatus.Empty
+  );
+
+  return (
+    <main className="space-y-4 p-8 text-gray-900">
+      <h1 className="text-2xl font-bold">챌린지 완주! 👏</h1>
+
+      <p>
+        총점 15점 중 <strong>10점</strong>을 획득하셨습니다.
+      </p>
+
+      {accuntInfoStatus === AccountInfoStatus.Empty ? (
+        <form
+          className="rounded-lg border p-4"
+          onSubmit={(e) => {
+            e.preventDefault();
+            setAccountInfoStatus(AccountInfoStatus.Filled);
+          }}
+        >
+          <h2 className="mb-3 text-lg font-bold">계좌 정보 입력하기</h2>
+
+          <div className="space-y-2">
+            <label className="block space-x-2">
+              <span>계좌번호</span>
+              <input
+                className="rounded border px-1 py-0.5"
+                type="text"
+                name="account-address"
+                placeholder="계좌번호 입력..."
+              />
+            </label>
+
+            <label className="block space-x-2">
+              <span>예금주</span>
+              <input
+                className="rounded border px-1 py-0.5"
+                type="text"
+                name="account-holder"
+                placeholder="예금주 입력..."
+              />
+            </label>
+
+            <label className="block space-x-2">
+              <span>은행</span>
+              <input
+                className="rounded border px-1 py-0.5"
+                type="text"
+                name="bank"
+                placeholder="은행 입력..."
+              />
+            </label>
+          </div>
+
+          <button
+            type="submit"
+            className="mt-4 w-full rounded-lg bg-sky-500 px-4 py-2 text-white"
+          >
+            입력
+          </button>
+        </form>
+      ) : (
+        <section className="rounded-lg border p-4">
+          [[등록된 계좌 정보]]
+          <button
+            type="button"
+            className="mt-4 w-full rounded-lg bg-sky-500 px-4 py-2 text-white"
+          >
+            환급 및 보상 받기
+          </button>
+        </section>
+      )}
+    </main>
+  );
+}
